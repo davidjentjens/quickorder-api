516ccbf2ad70cb14fffca9d859b21cda4731568f
David Maisonnette Jentjens <davidjentjens@gmail.com>
2023-12-01 15:28:10 -0300

    Implement Tests for the Dish Entity and the Mongoose connection

--- src/dishes/entities/dish.entity.spec.ts
+++ src/dishes/entities/dish.entity.spec.ts
@@ -1,17 +1,58 @@
-import { Dish } from "./dish.entity"
+import mongoose from "mongoose"
+import { Dish, DishSchema } from "./dish.entity"
 
 describe("Dish Tests", () => {
-    it("should create a dish", () => {
-        const dish = new Dish({
-            name: "Bolognese Sauce",
-            price: 10,
-            description: "A delicious sauce",
-            imageUrl: "http://testurl.test"
+
+    describe("Dish Class", () => {
+        it("should create a dish", () => {
+            const dish = new Dish({
+                name: "Bolognese Sauce",
+                price: 10,
+                description: "A delicious sauce",
+                imageUrl: "http://testurl.test"
+            })
+
+            expect(dish.name).toBe("Bolognese Sauce")
+            expect(dish.price).toBe(10)
+            expect(dish.description).toBe("A delicious sauce")
+            expect(dish.imageUrl).toBe("http://testurl.test")
+        })
+    })
+
+    describe("Using MongoDB", () => {
+        let conn: mongoose.Mongoose
+
+        beforeEach(async () => {
+            conn = await mongoose.connect(
+                "mongodb://root:root@localhost:27017/dishes_test?authSource=admin"
+            )
         })
 
-        expect(dish.name).toBe("Bolognese Sauce")
-        expect(dish.price).toBe(10)
-        expect(dish.description).toBe("A delicious sauce")
-        expect(dish.imageUrl).toBe("http://testurl.test")
+        afterEach(async () => {
+            await conn.disconnect()
+        })
+
+        it("should create a dish document", async () => {
+            const conn = await mongoose.connect(
+                "mongodb://root:root@localhost:27017/dishes_test?authSource=admin"
+            )
+
+            const DishModel = conn.model("Dish", DishSchema)
+            const dish = new DishModel({
+                name: "Bolognese Sauce",
+                price: 10,
+                description: "A delicious sauce",
+                imageUrl: "http://testurl.test"
+            })
+
+            await dish.save()
+
+            const createdDish = await DishModel.findOne(dish._id)
+
+            expect(createdDish.name).toBe("Bolognese Sauce")
+            expect(createdDish.price).toBe(10)
+            expect(createdDish.description).toBe("A delicious sauce")
+            expect(createdDish.imageUrl).toBe("http://testurl.test")
+        })
     })
 })
