a638027e97a9dfffa0026116746b99d1cc9ed086
David Maisonnette Jentjens <davidjentjens@gmail.com>
2023-12-01 15:24:08 -0300

    Implement Dish Entity with Mongoose

--- src/dishes/entities/dish.entity.ts
+++ src/dishes/entities/dish.entity.ts
@@ -1,3 +1,5 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
 export type DishProps = {
     name: string;
     price: number;
@@ -5,13 +7,23 @@ export type DishProps = {
     imageUrl: string;
 };
 
+@Schema()
 export class Dish {
     constructor(props: DishProps) {
         Object.assign(this, props);
     }
 
+    @Prop({ required: true })
     name: string;
+
+    @Prop({ required: true })
     price: number;
+
+    @Prop()
     description: string;
+
+    @Prop({ required: true })
     imageUrl: string;
 }
+
+export const DishSchema = SchemaFactory.createForClass(Dish);
